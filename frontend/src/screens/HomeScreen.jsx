3dbb1bc2dd62a5ff09c2e9c4dc0a529d4fee6818
nuke100pr <prakharmaurya700@gmail.com>
2024-04-04 00:27:22 +0530

    initial commits

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,15 @@
+import { Row, Col } from "react-bootstrap";
+
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+    
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
